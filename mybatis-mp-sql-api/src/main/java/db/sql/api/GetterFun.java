f6a707681ec6d4179ad0e5d8c79b1ed5f2121fab
Ai东 <abc-127@live.cn>
2024-06-05 12:57:13 +0800

    格式化

--- mybatis-mp-sql-api/src/main/java/db/sql/api/GetterFun.java
+++ mybatis-mp-sql-api/src/main/java/db/sql/api/GetterFun.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.function.Function;
 
 
-public interface GetterFun<T,R> extends Function<T, R>, Serializable {
+public interface GetterFun<T, R> extends Function<T, R>, Serializable {
 
 
 }
