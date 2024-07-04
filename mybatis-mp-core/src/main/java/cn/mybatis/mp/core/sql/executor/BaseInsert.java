fe028704cee6c10e6bb562e530376d216086905a
Ai东 <abc-127@live.cn>
2024-07-04 13:02:29 +0800

    优化

--- mybatis-mp-core/src/main/java/cn/mybatis/mp/core/sql/executor/BaseInsert.java
+++ mybatis-mp-core/src/main/java/cn/mybatis/mp/core/sql/executor/BaseInsert.java
@@ -1,22 +1,64 @@
 package cn.mybatis.mp.core.sql.executor;
 
+import cn.mybatis.mp.core.db.reflect.TableFieldInfo;
+import cn.mybatis.mp.core.db.reflect.TableInfo;
+import cn.mybatis.mp.core.db.reflect.Tables;
+import cn.mybatis.mp.core.mybatis.mapper.context.MybatisParameter;
 import cn.mybatis.mp.core.sql.MybatisCmdFactory;
+import db.sql.api.Cmd;
 import db.sql.api.Getter;
+import db.sql.api.impl.cmd.Methods;
 import db.sql.api.impl.cmd.basic.TableField;
 import db.sql.api.impl.cmd.executor.AbstractInsert;
+import db.sql.api.impl.tookit.LambdaUtil;
+import org.apache.ibatis.type.UnknownTypeHandler;
+
+import java.util.*;
 
 public abstract class BaseInsert<T extends BaseInsert<T>> extends AbstractInsert<T, MybatisCmdFactory> {
     public BaseInsert() {
         super(new MybatisCmdFactory());
     }
 
-    /**************以下为去除警告************/
+    private Getter<?>[] fields;
+
+    private Map<Class<?>, TableInfo> tableInfoMap = new HashMap<>();
 
     @Override
     @SafeVarargs
     public final <T2> T field(Getter<T2>... fields) {
+        this.fields = fields;
         return super.field(fields);
     }
+
+    @Override
+    public T values(List<Object> values) {
+        if (Objects.isNull(this.fields)) {
+            return super.values(values);
+        }
+        List<Cmd> cmdValues = new ArrayList<>(values.size());
+        for (int i = 0; i < values.size(); i++) {
+            Object value = values.get(i);
+            if (value instanceof Cmd || value instanceof MybatisParameter) {
+                cmdValues.add(Methods.convert(value));
+                continue;
+            }
+            LambdaUtil.LambdaFieldInfo fieldInfo = LambdaUtil.getFieldInfo(this.fields[i]);
+            TableInfo tableInfo = tableInfoMap.computeIfAbsent(fieldInfo.getType(), key -> {
+                return Tables.get(key);
+            });
+
+            TableFieldInfo tableFieldInfo = tableInfo.getFieldInfo(fieldInfo.getName());
+            if (tableFieldInfo.getTableFieldAnnotation().typeHandler() != UnknownTypeHandler.class) {
+                value = MybatisParameter.create(value, tableFieldInfo.getTableFieldAnnotation().typeHandler(), tableFieldInfo.getTableFieldAnnotation().jdbcType());
+            }
+            cmdValues.add(Methods.convert(value));
+        }
+        this.$values(cmdValues);
+        return (T) this;
+    }
+
+    /**************以下为去除警告************/
 
     @Override
     @SafeVarargs
--- mybatis-mp-core/src/main/java/cn/mybatis/mp/core/sql/executor/BaseUpdate.java
+++ mybatis-mp-core/src/main/java/cn/mybatis/mp/core/sql/executor/BaseUpdate.java
@@ -1,17 +1,25 @@
 package cn.mybatis.mp.core.sql.executor;
 
+import cn.mybatis.mp.core.db.reflect.TableFieldInfo;
+import cn.mybatis.mp.core.db.reflect.TableInfo;
+import cn.mybatis.mp.core.db.reflect.Tables;
 import cn.mybatis.mp.core.logicDelete.LogicDeleteUtil;
+import cn.mybatis.mp.core.mybatis.mapper.context.MybatisParameter;
 import cn.mybatis.mp.core.sql.MybatisCmdFactory;
 import cn.mybatis.mp.core.tenant.TenantUtil;
 import cn.mybatis.mp.core.util.ForeignKeyUtil;
 import db.sql.api.Cmd;
+import db.sql.api.Getter;
 import db.sql.api.cmd.basic.IDataset;
 import db.sql.api.cmd.basic.IDatasetField;
 import db.sql.api.impl.cmd.basic.Table;
 import db.sql.api.impl.cmd.executor.AbstractUpdate;
 import db.sql.api.impl.cmd.struct.On;
 import db.sql.api.impl.cmd.struct.Where;
+import db.sql.api.impl.tookit.LambdaUtil;
+import org.apache.ibatis.type.UnknownTypeHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -24,6 +32,26 @@ public abstract class BaseUpdate<T extends BaseUpdate<T>> extends AbstractUpdate
 
     public BaseUpdate(Where where) {
         super(where);
+    }
+
+    private Map<Class<?>, TableInfo> tableInfoMap = new HashMap<>();
+
+    @Override
+    public <G> T set(Getter<G> field, Object value) {
+        Objects.requireNonNull(value);
+        if (value instanceof Cmd || value instanceof MybatisParameter) {
+            return super.set(field, value);
+        }
+        LambdaUtil.LambdaFieldInfo fieldInfo = LambdaUtil.getFieldInfo(field);
+        TableInfo tableInfo = tableInfoMap.computeIfAbsent(fieldInfo.getType(), key -> {
+            return Tables.get(key);
+        });
+
+        TableFieldInfo tableFieldInfo = tableInfo.getFieldInfo(fieldInfo.getName());
+        if (tableFieldInfo.getTableFieldAnnotation().typeHandler() != UnknownTypeHandler.class) {
+            value = MybatisParameter.create(value, tableFieldInfo.getTableFieldAnnotation().typeHandler(), tableFieldInfo.getTableFieldAnnotation().jdbcType());
+        }
+        return super.set(field, value);
     }
 
     @Override
