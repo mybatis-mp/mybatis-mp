908499255fceefca725221e2a28e4c4d2f126562
Ai东 <abc-127@live.cn>
2024-08-21 11:13:14 +0800

    优化

--- mybatis-mp-sql-api-impl/src/main/java/db/sql/api/impl/tookit/SQLOptimizeUtils.java
+++ mybatis-mp-sql-api-impl/src/main/java/db/sql/api/impl/tookit/SQLOptimizeUtils.java
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
 public final class SQLOptimizeUtils {
 
     public static int getStringBuilderCapacity(List<Cmd> cmds) {
-        return 100;
+        return 16;
     }
 
     private static boolean isCanRemoveLeftJoin(Join current, List<Join> joinList, boolean forClass, Map<Class, Cmd> classCmdMap) {
