6453bbadc0e2fb6299c7c57353deeab3076ef5bb
Ai东 <abc-127@live.cn>
2024-08-29 10:53:06 +0800

    优化

--- mybatis-mp-core/src/main/java/cn/mybatis/mp/core/mybatis/configuration/MybatisConfiguration.java
+++ mybatis-mp-core/src/main/java/cn/mybatis/mp/core/mybatis/configuration/MybatisConfiguration.java
@@ -3,7 +3,6 @@ package cn.mybatis.mp.core.mybatis.configuration;
 
 import cn.mybatis.mp.core.NotTableClassException;
 import cn.mybatis.mp.core.mybatis.mapper.BasicMapper;
-import cn.mybatis.mp.core.mybatis.mapper.DbRunner;
 import cn.mybatis.mp.core.mybatis.mapper.MybatisMapper;
 import cn.mybatis.mp.core.mybatis.resultset.MybatisDefaultResultSetHandler;
 import cn.mybatis.mp.core.mybatis.typeHandler.EnumTypeHandler;
@@ -97,21 +96,11 @@ public class MybatisConfiguration extends Configuration {
         }
     }
 
-    private void addDbRunner() {
-        if (!this.mapperRegistry.hasMapper(DbRunner.class)) {
-            super.addMapper(DbRunner.class);
-        }
-    }
-
     @Override
     public <T> void addMapper(Class<T> type) {
         //添加基础 BasicMapper
         if (!this.hasMapper(BasicMapper.class)) {
             this.addBasicMapper(BasicMapper.class);
-        }
-        //添加
-        if (!this.hasMapper(DbRunner.class)) {
-            this.addDbRunner();
         }
 
         if (MybatisMapper.class.isAssignableFrom(type)) {
