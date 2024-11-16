9fb6d3c76c0e520567b2aaeb4312053047f3ea6e
Ai东 <abc-127@live.cn>
2024-11-16 22:04:08 +0800

    1.7.6-RC2

--- mybatis-mp-core/src/test/java/com/mybatis/mp/core/test/testCase/TestDataSource.java
+++ mybatis-mp-core/src/test/java/com/mybatis/mp/core/test/testCase/TestDataSource.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 
 public class TestDataSource {
 
-    public static final DbType DB_TYPE = DbType.KING_BASE;
+    public static final DbType DB_TYPE = DbType.H2;
 
     public static final String TIME_ZONE = "Asia/Shanghai";
 
