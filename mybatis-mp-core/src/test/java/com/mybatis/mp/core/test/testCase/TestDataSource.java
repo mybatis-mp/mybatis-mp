a16466d9a5f8cfc5f1ee2708849b6636219d02bf
Ai东 <abc-127@live.cn>
2025-01-17 10:58:02 +0800

    默认H2

--- mybatis-mp-core/src/test/java/com/mybatis/mp/core/test/testCase/TestDataSource.java
+++ mybatis-mp-core/src/test/java/com/mybatis/mp/core/test/testCase/TestDataSource.java
@@ -29,7 +29,7 @@ import java.util.Objects;
 
 public class TestDataSource {
 
-    public static final DbType DB_TYPE = DbType.OPEN_GAUSS;
+    public static final DbType DB_TYPE = DbType.H2;
 
     public static final String TIME_ZONE = "Asia/Shanghai";
 
